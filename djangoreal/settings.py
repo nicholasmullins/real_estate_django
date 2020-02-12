e00655b3586697ed128bcf1dcedd4f108e1550d1
Home <home@Antique.local>
2020-02-11 17:02:54 -0700

    Added local setting import

--- djangoreal/settings.py
+++ djangoreal/settings.py
@@ -149,4 +149,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'thenewyorkconcierge@gmail.com'
 EMAIL_HOST_PASSWORD = 'lennon80'
-EMAIL_USE_TLS=True
+EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
