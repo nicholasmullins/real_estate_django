6eb756c3ed5e40f0c0cf898161fe4fda41a695ca
Home <home@Antique.local>
2020-02-11 17:18:02 -0700

    Removed email from settings completely

--- djangoreal/settings.py
+++ djangoreal/settings.py
@@ -144,12 +144,6 @@ MESSAGE_TAGS = {
 
 }
 
-# Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'thenewyorkconcierge@gmail.com'
-EMAIL_HOST_PASSWORD = 'lennon80'
-EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
